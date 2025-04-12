0e915ad09c6179167b160b35b2fa68e9bd61fd62
abeRodz <abrahamrodz17@unitec.edu>
2025-04-12 14:37:05 -0600

    FIX: added precommit validations

--- test/test_leds.c
+++ test/test_leds.c
@@ -28,91 +28,81 @@ SPDX-License-Identifier: MIT
 #include "unity.h"
 #include "leds.h"
 
-
 /* === Macros definitions ====================================================================== */
 
 /* === Private data type declarations ========================================================== */
 static uint16_t virtual_leds = 0xFFFF;
 /* === Private variable declarations =========================================================== */
 
-/* === Private function declarations =========================================================== */\
+/* === Private function declarations =========================================================== */
 
-void setUp(void){
+void setUp(void) {
 
     LedsInit(&virtual_leds);
 }
 
-/** @test initiates all LEDs as turned off. */ 
+/** @test initiates all LEDs as turned off. */
 void test_init_every_led_turned_off(void) {
     uint16_t virtual_leds = 0xFFFF;
 
     LedsInit(&virtual_leds);
 
-    TEST_ASSERT_EQUAL_HEX16(0x0000,virtual_leds);
+    TEST_ASSERT_EQUAL_HEX16(0x0000, virtual_leds);
 }
 
-/** @test initiates all LEDs as turned off. */ 
-void test_turn_on_single_led(void){
+/** @test initiates all LEDs as turned off. */
+void test_turn_on_single_led(void) {
 
     LedTurnOnSingle(4);
 
-    TEST_ASSERT_EQUAL_HEX16(0x0008,virtual_leds);
-
+    TEST_ASSERT_EQUAL_HEX16(0x0008, virtual_leds);
 }
 
-/** @test turns off a single LED. */ 
-void test_turn_off_single_led(void){
+/** @test turns off a single LED. */
+void test_turn_off_single_led(void) {
     LedTurnOnSingle(4);
     LedTurnOffSingle(4);
-    TEST_ASSERT_EQUAL_HEX16(0x0000,virtual_leds);
-
+    TEST_ASSERT_EQUAL_HEX16(0x0000, virtual_leds);
 }
 
-/** @test turns off a multiple designated LEDs. */ 
-void test_turn_on_off_multiple_led(void){
+/** @test turns off a multiple designated LEDs. */
+void test_turn_on_off_multiple_led(void) {
     LedTurnOnSingle(4);
     LedTurnOnSingle(6);
 
     LedTurnOffSingle(4);
     LedTurnOffSingle(8);
 
-    TEST_ASSERT_EQUAL_HEX16(0x0020,virtual_leds);
-
+    TEST_ASSERT_EQUAL_HEX16(0x0020, virtual_leds);
 }
 
-/** @test turns on all LEDs. */ 
-void test_turn_on_all_leds(void){
+/** @test turns on all LEDs. */
+void test_turn_on_all_leds(void) {
     LedsTurnOnAll();
 
     TEST_ASSERT_EQUAL_HEX16(0xFFFF, virtual_leds);
-
 }
 
-/** @test turns off all LEDs. */ 
-void test_turn_off_all_leds(void){
+/** @test turns off all LEDs. */
+void test_turn_off_all_leds(void) {
     LedsTurnOffAll();
 
     TEST_ASSERT_EQUAL_HEX16(0x0000, virtual_leds);
-
 }
 
-/** @test gets current ON status */ 
-void test_get_on_status(void){
+/** @test gets current ON status */
+void test_get_on_status(void) {
     LedTurnOnSingle(4);
 
     TEST_ASSERT_TRUE(isLedOn(4));
-
 }
-/** @test gets current OFF status */ 
-void test_get_on_off_status(void){
+/** @test gets current OFF status */
+void test_get_on_off_status(void) {
     LedTurnOnSingle(4);
     LedTurnOffSingle(4);
 
     TEST_ASSERT_FALSE(isLedOn(4));
-
 }
 /* === Public function implementation ========================================================== */
-
-
 
 /* === End of documentation ==================================================================== */
